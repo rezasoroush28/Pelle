9034e3562bba6b5b4120faa6fd9563fb3d3f3fd7
reza soroush <rezasoroush28@gmail.com>
2024-12-05 02:38:26 +0330

    dsspldp

--- Application/Class1.cs
+++ Application/Class1.cs
@@ -1,6 +1,12 @@
-﻿namespace Application
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
 {
-    public class Class1
+    public static class JwtHelper
     {
 
     }
---
+++ Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Core.Entities.User> Users { get; set; }
+
+        // Optional: Fluent API or configurations
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Seed data, configurations, etc.
+            modelBuilder.Entity<User>().HasData(
+                new User { Id = 1, Username = "user1", Password = "password1" },
+                new User { Id = 2, Username = "user2", Password = "password2" }
+            );
+        }
+    }
+}
