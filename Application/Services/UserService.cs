76764983f7ac6036b6f70a2d2529dc1bbdd1f77d
reza soroush <rezasoroush28@gmail.com>
2024-12-05 05:22:08 +0330

    sfsgf

--- Application/Services/UserService.cs
+++ Application/Services/UserService.cs
@@ -1,7 +1,29 @@
-﻿namespace Application.Services
+﻿using Core.Entities;
+using Core.Interfaces;
+using System.Linq;
+
+namespace Application.Services
 {
     public class UserService
     {
+        private readonly IRepository<User> _userRepository;
+        public UserService(IRepository<User> userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<string>  LoginAsync(string username, string password)
+        {
+            var users = await _userRepository.GetAllAsync();
+            var foundUser = users.FirstOrDefault(u => (u.Username == username && u.Password == password));
+            if (foundUser != null)
+            {
+                return null;
+            }
+
+            return JwtHelper.GenerateToken(username);
+        }
+
 
     }
 }
--- webAPI/Program.cs
+++ webAPI/Program.cs
@@ -1,7 +1,30 @@
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
+var jwtSettings = builder.Configuration.GetSection("Jwt");
+var key = Encoding.ASCII.GetBytes(jwtSettings["Key"]);
 
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = "Bearer";
+    options.DefaultChallengeScheme = "Bearer";
+})
+.AddJwtBearer("Bearer", options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidIssuer = jwtSettings["Issuer"], // From appsettings
+        ValidAudience = jwtSettings["Audience"], // From appsettings
+        ValidateLifetime = true
+    };
+});
 // Add services to the container.
 
 builder.Services.AddControllers();
